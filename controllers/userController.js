089e99b1e77201b87e4fe95c650edc34f9dc7a0d
Sunima Rai <np01cp4s210154@islingtoncollege.edu.np>
2023-01-07 22:18:17 +0545

    added login feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,20 +1,68 @@
 const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.register = (req, res, next) => {
-  let user = new User({
-    name: req.body.name,
-    username: req.body.username,
-    password: req.body.password,
-    role: req.body.role,
-    gender: req.body.gender,
-    vehicleType: req.body.vehicleType,
-    vehiclePlateNumber: req.body.vehiclePlateNumber,
-  });
-  user.save().then((data) => {
-    res.json({
-      status: 200,
-      message: "user registered successfully!",
-      data,
+  bcrypt.hash(req.body.password, 10, function (err, hashed) {
+    if (err) {
+      res.json({
+        status: "500",
+        message: err,
+      });
+    }
+    let user = new User({
+      name: req.body.name,
+      username: req.body.username,
+      password: hashed,
+      role: req.body.role,
+      gender: req.body.gender,
+      vehicleType: req.body.vehicleType,
+      vehiclePlateNumber: req.body.vehiclePlateNumber,
     });
+    user.save().then((data) => {
+      res.json({
+        status: 200,
+        message: "user registered successfully!",
+        data,
+      });
+    });
+  });
+};
+
+exports.login = (req, res, next) => {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  User.findOne({ username }).then((user) => {
+    if (user) {
+      bcrypt.compare(password, user.password, function (err, ismatch) {
+        if (err) {
+          res.json({
+            message: err,
+          });
+        }
+        if (ismatch) {
+          let token = jwt.sign({ name: user._id }, "thesecrettoken", {
+            expiresIn: "10min",
+          });
+          let refreshToken = jwt.sign({ name: user._id }, "thesecrettoken", {
+            expiresIn: "20min",
+          });
+          res.json({
+            message: "Login successful!",
+            token,
+            refreshToken,
+          });
+        } else {
+          res.json({
+            message: "Password does not matched!",
+          });
+        }
+      });
+    } else {
+      res.json({
+        message: "NO user found!",
+      });
+    }
   });
 };
